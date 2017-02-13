7ba5ec370645e92f0d284a868d0b5d388e4651a4
unknown <djshadle@SH308-29.coe.int>
2017-02-13 13:14:52 -0600

    Commit: The tutor class has been made. The tutee class will be made next.

---
+++ Tutor Master/Tutor Master/Tutor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tutor_Master
+{
+    class Tutor : Profile
+    {
+        //all the private data
+        private string[] approvedCourses;
+        //private schedule tutorSchedule;
+        //We will wait on this until the tutor class is made
+
+
+        //all the public functions
+        public void manageSchedule() {
+        
+        }
+        public void confirmTuteeRequest() { 
+        
+        }
+        public void declineTuteeRequest() { 
+        
+        }
+        public void sendApptRequest() { 
+        
+        }
+    }
+}
