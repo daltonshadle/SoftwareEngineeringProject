235acf2c0895d26af1cc9004906c81c1fbbec8f7
unknown <djshadle@SH308-29.coe.int>
2017-02-13 13:23:00 -0600

    Commit: Tutee class complete. Appointment and Schedule classes will be next as all the previous classes need those.

---
+++ Tutor Master/Tutor Master/Tutee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tutor_Master
+{
+    class Tutee : Profile
+    {
+        //all the private data
+        private string[] courses;
+        //private schedule tuteeSchedule;
+        //Will wait on this one until the schedule class is done
+
+        //all the public functions
+        public void manageSchedule() { 
+        
+        }
+        public void confirmTutorRequest() { 
+        
+        }
+        public void declineTutorRequest() { 
+        
+        }
+        public void sendApptRequest() { 
+        
+        }
+    }
+}
