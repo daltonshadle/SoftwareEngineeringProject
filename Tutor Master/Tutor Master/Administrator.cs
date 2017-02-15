31de1c0d7c1c43d2fd1d5da7ddfdb148b0c76ce2
unknown <djshadle@SH308-29.coe.int>
2017-02-15 13:48:58 -0600

    Commit: Added the admin class and need to finish that next time.

---
+++ Tutor Master/Tutor Master/Administrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tutor_Master
+{
+    class Administrator
+    {
+        //all the private data
+
+
+        //all the public functions
+
+        //constructor
+        public Administrator() { 
+        
+        }
+
+        public void manageSchedule() { 
+        
+        }
+    }
+}
