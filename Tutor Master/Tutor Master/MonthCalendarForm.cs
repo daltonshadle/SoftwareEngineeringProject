315b5e1bf907f3da5df6e1ca54a3ffdb7bb3892c
unknown <sfmichalek@SH308-29.coe.int>
2017-03-26 16:07:50 -0500

    Now the calendar displays the current day's appointments as soon as the calendar is pulled up

--- Tutor Master/Tutor Master/MonthCalendarForm.cs
+++ Tutor Master/Tutor Master/MonthCalendarForm.cs
@@ -27,17 +27,6 @@ namespace Tutor_Master
 
             Database db = new Database();
             appointmentList = db.getDailyAppointments(username);
-            /*if (appointmentList.Count != 0) {
-                panel1.Visible = true;
-
-                for (int i = 0; i < appointmentList.Count; i++)
-                {
-                    AppointmentBlock a = new AppointmentBlock(appointmentList[i]);
-                    panel1.Controls.Add(a);
-                }
-            }
-            else 
-                panel1.Visible = false;*/
 
             //Fill the calendar with bolded dates
             monthCalendar1.profileMonthCalendar.BoldedDates = new System.DateTime[] { };
@@ -46,6 +35,38 @@ namespace Tutor_Master
                 DateTime appointmentDate = appointmentList[i].getStartTime();
                 monthCalendar1.profileMonthCalendar.AddBoldedDate(appointmentDate);
             }
+
+
+            //Set the panel to display once the calendar is pulled up
+            //would clean up code a shit-ton if i could make a function, but e.Start is the problem.
+
+            //Fill up the list for each day.
+            List<Appointment> dailyAppointments = new List<Appointment>();
+            for (int i = 0; i < appointmentList.Count; i++)
+            {
+                if (appointmentList[i].getStartTime().Date == DateTime.Now.Date)
+                {
+                    dailyAppointments.Add(appointmentList[i]);
+                }
+            }
+
+            //Display all of the daily apps
+            if (dailyAppointments.Count > 0)
+            {
+
+                AppointmentBlock a;
+                for (int j = 0; j < dailyAppointments.Count; j++)
+                {
+                    a = new AppointmentBlock(appointmentList[j]);
+                    int x = makeX(j);
+                    int y = makeY(j);
+                    a.Location = new Point(x, y);
+                    panel1.Controls.Add(a);
+                }
+                panel1.Visible = true;
+            }
+            else
+                panel1.Visible = false;
 
         }
 
